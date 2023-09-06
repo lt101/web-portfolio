cf9845f95205740fddb86221d30ed526feb80245
Liem <liem.truong@polymtl.ca>
2023-09-05 21:42:56 -0400

    remove unused import

--- src/App.js
+++ src/App.js
@@ -3,7 +3,6 @@ import "./App.css";
 import Header from "./components/header/Header";
 import Home from "./components/home/Home";
 import About from "./components/about/About";
-import Skills from "./components/skills/Skills";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import ScrollUp from "./components/scrollup/ScrollUp";
