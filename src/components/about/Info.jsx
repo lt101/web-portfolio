2b417b8695a0c100b438bddacb5652c402168162
Liem <liem.truong@polymtl.ca>
2023-09-08 22:42:30 -0400

    switch title in about sections

--- src/components/about/Info.jsx
+++ src/components/about/Info.jsx
@@ -5,14 +5,14 @@ const Info = () => {
     <div className="about_info grid">
       <div className="about_box">
         <i className="bx bx-briefcase-alt about_icon"></i>
-        <h3 className="about_title">Fullstack developer intern</h3>
-        <span className="about_subtitle">helloDarwin</span>
+        <h3 className="about_title">helloDarwin</h3>
+        <span className="about_subtitle">Fullstack developer intern</span>
         <p className="about_date">Summer 2022</p>
       </div>
       <div className="about_box">
         <i className="bx bx-briefcase-alt about_icon"></i>
-        <h3 className="about_title">Fullstack developer intern</h3>
-        <span className="about_subtitle">Optimum Financial Group</span>
+        <h3 className="about_title">Optimum Financial Group</h3>
+        <span className="about_subtitle">Fullstack developer intern</span>
         <p className="about_date">Summer 2021</p>
       </div>
     </div>
