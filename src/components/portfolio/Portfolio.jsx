d394c99a34b601687d8720c6b08c5c309a626366
Liem <liem.truong@polymtl.ca>
2023-09-10 20:25:39 -0400

    add rough notation

--- src/components/portfolio/Portfolio.jsx
+++ src/components/portfolio/Portfolio.jsx
@@ -1,14 +1,35 @@
-import React from "react";
+import React, { useRef, useState, useEffect } from "react";
 import "./portfolio.css";
 import Works from "./Works";
+import { useInView } from "framer-motion";
+import { RoughNotation } from "react-rough-notation";
 
 const Portfolio = () => {
+  const ref = useRef(null);
+  const [roughAnimate, setRoughAnimate] = useState(false);
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      setRoughAnimate(true);
+    }
+  }, [isInView]);
   return (
-    <section className="portfolio section container" id="portfolio">
+    <section className="portfolio section container" id="portfolio" ref={ref}>
       <div className="work_header">
         Feel free to explore some of{" "}
-        <span className="section_title">My Work</span>
+        <RoughNotation
+          type="box"
+          color="#A52A2A"
+          padding={[25, 15, 10, 0]}
+          strokeWidth={3}
+          animationDelay={1100}
+          show={roughAnimate}
+        >
+          <span className="section_title">My Work</span>
+        </RoughNotation>
       </div>
+
       <Works />
     </section>
   );
--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import "./skills.css";
 import ReactLogo from "../../assets/react_logo_icon_144942.svg";
 import AngularLogo from "../../assets/angular_logo_icon_145496.svg";
@@ -8,15 +8,37 @@ import JavaLogo from "../../assets/2333414-code-java_85589.svg";
 import PythonLogo from "../../assets/Python_icon-icons.com_68975.svg";
 import NodeJSLogo from "../../assets/node_icon_196185.svg";
 import MongoDBLogo from "../../assets/mongodb_logo_icon_248434.svg";
+import { useInView } from "framer-motion";
+import { RoughNotation } from "react-rough-notation";
 
 const Skills = () => {
+  const ref = useRef(null);
+  const [roughAnimate, setRoughAnimate] = useState(false);
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      setRoughAnimate(true);
+    }
+  }, [isInView]);
   return (
     <section className="skills section container" id="skills">
       <div className="skills_header">
-        Here are some of the <span className="section_title">Skills</span> and
-        technologies that I use.
+        Here are some of the{" "}
+        <RoughNotation
+          type="circle"
+          color="#00CED1"
+          strokeWidth={3}
+          padding={[40, 0, 10, 0]}
+          show={roughAnimate}
+          animationDelay={700}
+        >
+          <span className="section_title">Skills</span>
+        </RoughNotation>{" "}
+        and technologies that I use.
       </div>
-      <div className="skills_container">
+
+      <div className="skills_container" ref={ref}>
         <div className="skills_group grid">
           <div className="skills_item">
             <img src={ReactLogo} alt="react logo" />
