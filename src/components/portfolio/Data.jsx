0fb9f14637650896a9455fdfb5d8370f8864a088
Liem <liem.truong@polymtl.ca>
2023-10-10 21:46:18 -0400

    update works section

--- src/components/portfolio/Data.jsx
+++ src/components/portfolio/Data.jsx
@@ -12,7 +12,8 @@ export const projectsData = [
     description:
       "A centralized job application tracker to help you search for the next step of your career. Create new job applications and update them as you progress in the recruitment process. Good luck in your search!",
     tags: "MERN | React | Redux | Node.js | MongoDB | Express",
-    link: "https://github.com/lt101/job-tracker",
+    code: "https://github.com/lt101/job-tracker",
+    demo: "https://next-ep-h8c3.onrender.com",
   },
   {
     id: 2,
@@ -21,7 +22,7 @@ export const projectsData = [
     description:
       "Early in my software development journey, I was fascinated by real-time communication. This application pays homage to my fascination, showcasing the interaction between React and the Firebase API.",
     tags: "React | Javascript | Firebase",
-    link: "https://github.com/lt101/react-firebase-chat",
+    code: "https://github.com/lt101/react-firebase-chat",
   },
   {
     id: 3,
@@ -30,7 +31,7 @@ export const projectsData = [
     description:
       "An application that allows you to manage an inventory of anything you want. Add, edit and delete items. Simple.",
     tags: "C# | .NET | MVC | CRUD",
-    link: "https://github.com/lt101/.net-inventory-manager",
+    code: "https://github.com/lt101/.net-inventory-manager",
   },
   {
     id: 4,
@@ -39,7 +40,7 @@ export const projectsData = [
     description:
       "This is a fancy version of the popular and timeless game of Scrabble. Play the original game, or try completing objectives for extra points. The game can be generated as a desktop app, a mobile app, or simply played on a web browser.",
     tags: "Angular | Typescript | CSS",
-    link: "https://github.com/lt101/scrabble-v2-desktop-mobile-app",
+    code: "https://github.com/lt101/scrabble-v2-desktop-mobile-app",
   },
 
   {
@@ -48,7 +49,7 @@ export const projectsData = [
     title: "Data analysis tool",
     description:
       "For my final university project, I developed a data protection app in partnership with LGS, an IBM company. The application allows you to clean, format, analyze and anonymize datasets. You can also train a machine learning model with the brute and anonymized datasets and compare their performances.",
-    tags: "Python | Pandas | Streamlit",
-    link: "https://github.com/lt101/IBM-data-privacy-protection",
+    tags: "Python | Pandas | Plotly | Streamlit",
+    code: "https://github.com/lt101/IBM-data-privacy-protection",
   },
 ];
--- src/components/portfolio/WorksItem.jsx
+++ src/components/portfolio/WorksItem.jsx
@@ -8,10 +8,17 @@ const WorksItem = ({ item }) => {
         <h3 className="work_title">{item.title}</h3>
         <div className="work_description">{item.description}</div>
         <div className="work_tags">{item.tags}</div>
-
-        <a href={item.link} target="_blank" className="work_button">
-          View code <i className="bx bx-right-arrow-alt work_button-icon"></i>
-        </a>
+        <div className="work_links">
+          {item.demo && (
+            <a href={item.demo} target="_blank" className="work_button">
+              <i className="uil uil-external-link-alt work_button-icon"></i> Try
+              it out
+            </a>
+          )}
+          <a href={item.code} target="_blank" className="work_button">
+            <i className="uil uil-github work_button-icon"></i>View code
+          </a>
+        </div>
       </div>
     </div>
   );
